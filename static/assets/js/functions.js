deb269c647a1faf91df4de925adab4baab414c4b
ODMSTAG <135626996+ODMSTAG@users.noreply.github.com>
2024-09-03 12:16:35 -0400

    Update functions.js

--- static/assets/js/functions.js
+++ static/assets/js/functions.js
@@ -113,7 +113,7 @@ function tabCloak() {
 }
 
 function disableTabCloak() {
-  var newTitle = "Doge | V4";
+  var newTitle = "Ezic | V11";
   var newIcon = "/assets/img/doge.jpg";
 
   localStorage.setItem("tabTitle", newTitle);
